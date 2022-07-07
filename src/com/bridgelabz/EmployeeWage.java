43c54c8aab3763ca52e36c37147f577d18597058
sujit <sujitbokade2014@gmail.com>
2022-07-07 17:00:30 +0530

    Employee Attendance

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,8 +1,16 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+    static final int Employee_Present=1;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage !");
+        double empCheck=Math.floor(Math.random()*10) % 2;
+            if(empCheck==Employee_Present)
+                System.out.println("employee is present");
+            else
+                System.out.println("employee is absent");
+
+        }
     }
-    }
+
 
