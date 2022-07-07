e875c7d319024f13f5eef788109b5a118af18616
sujit <sujitbokade2014@gmail.com>
2022-07-07 17:12:52 +0530

    Daily Employee Wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -2,14 +2,21 @@ package com.bridgelabz;
 
 public class EmployeeWage {
     static final int Employee_Present=1;
+    static final int Wage_per_Hr=20;
+    static final int Full_Day_Hr=16;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage !");
         double empCheck=Math.floor(Math.random()*10) % 2;
-            if(empCheck==Employee_Present)
-                System.out.println("employee is present");
-            else
-                System.out.println("employee is absent");
 
+        int dailywage=0;
+        if(empCheck==Employee_Present) {
+            System.out.println("employee is present");
+            dailywage=Wage_per_Hr*Full_Day_Hr;
+        }
+            else {
+            System.out.println("employee is absent");
+        }
+        System.out.println("Daily Wage is "+dailywage);
         }
     }
 
