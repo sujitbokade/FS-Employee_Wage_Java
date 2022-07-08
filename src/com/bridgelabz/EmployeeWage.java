add85b42373ba56ea570e69b939524f344a70a4a
sujit <sujitbokade2014@gmail.com>
2022-07-08 13:36:50 +0530

    Update Code

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,21 +1,21 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
-    static final int Employee_Present=1;
-    static final int IS_Part_Time=2;
-    static final int Wage_per_Hr=20;
-    static final int Full_Day_Hr=16;
-    static final int Part_Time=8;
+    static final int IS_FULL_TIME=1;
+    static final int IS_PART_TIME=2;
+    static final int WAGE_PER_HR=20;
+    static final int FULL_DAY_HR=16;
+    static final int PART_TIME_HR=8;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage !");
         double empCheck=Math.floor(Math.random()*10) % 3;
         int dailywage=0;
-        if(empCheck==Employee_Present) {
+        if(empCheck==IS_FULL_TIME) {
             System.out.println("employee is full time");
-            dailywage=Wage_per_Hr * Full_Day_Hr;
-        } else if (empCheck==IS_Part_Time) {
+            dailywage=WAGE_PER_HR * FULL_DAY_HR;
+        } else if (empCheck==IS_PART_TIME) {
             System.out.println("employee is part time");
-            dailywage=Wage_per_Hr * Part_Time;
+            dailywage=WAGE_PER_HR * PART_TIME_HR;
         }
         else {
             System.out.println("employee is absent");
